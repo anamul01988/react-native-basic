a7472ecaded80061be4528a434e9f6b6ac0565b6
anamul01988 <anamulhaque0198877@gmail.com>
2024-03-05 01:00:40 +0600

    login page is designed

--- App.js
+++ App.js
@@ -1,47 +1,31 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import {
-  Image,
-  ScrollView,
+  // Button,
+  // Platform,
+  // StatusBar,
   StyleSheet,
-  Text,
-  TextInput,
+  // Text,
   View,
 } from "react-native";
-
-const getFullName = (fn, mn, ln) => {
-  return fn + " " + mn + " " + ln;
-};
+import { LoginScreen } from "./src/screens";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Try editing me! 🎉</Text>
-      <StatusBar style="auto" />
-      <ScrollView>
-        <Text>Hey</Text>
-        <View>
-          <Text>More Hey</Text>
-          <Image
-            source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
-            style={{ width: 200, height: 200 }}
-          />
-        </View>
-        <TextInput
-          style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
-          defaultValue="hey input"
-        />
-        <Text> Hello I am {getFullName("Anamul", "Haque", "Shanto")}</Text>
-        {/* qustion react import carai kaj korlo kintu docs a to bola react import na korle kaj korbe nah jemon https://reactnative.dev/docs/intro-react */}
-      </ScrollView>
-    </View>
-  );
+  return <LoginScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "space-between",
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // padding: 20,
+    // margin: 10,
+  },
+  top: {
+    flex: 1,
+    backgroundColor: "yellow",
+    borderWidth: 5,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
 });
